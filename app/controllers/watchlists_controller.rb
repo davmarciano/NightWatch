d8de25ec43489136a09c57f9f824d5e301b97c5e
Josefine Theden-Schow <josi.theden@gmx.de>
2019-12-01 17:44:38 +0200

    add routes

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -2,4 +2,7 @@ class WatchlistsController < ApplicationController
   def index
    @watchlists = Watchlist.all
   end
+
+  def follow
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-  get 'watchlists/index'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :watchlists, only: [:index]
+  resources :watchlists
+  resources :movies, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:edit, :update, :destroy]
+
 
   namespace :profile do
-    resources :watchlists, only: [:index]
+    get "/home", to: "watchlists#home"
+    resources :watchlists, only: [:index] do
+      resources :movies, only: [:index]
+    end
   end
 end
