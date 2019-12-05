c7e87a0cdc4ae826f94ed1964ad47f91c4f6a383
Marie Heller <MarieHeller@hotmail.de>
2019-12-05 11:53:09 +0200

    Watchlists now display nicely, if only one or two. Modals are closed on click

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,8 +1,40 @@
 const slick = () => {
-  $('.watchlists-line-wrapper').slick({
+  const count = parseInt(document.querySelector(".my-watchlists-line-wrapper").dataset.watchlistCount, 10)
+  let slides;
+  if (count >= 3) {
+    slides = 3
+  } else if (count === 2) {
+    slides = 2
+  } else {
+    slides = 1
+  }
+  $('.my-watchlists-line-wrapper').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: slides,
+    slidesToScroll: slides,
+    dots: true
+  });
+};
+
+
+
+
+const slickFollowed = () => {
+
+  const count = parseInt(document.querySelector(".followed-watchlists-line-wrapper").dataset.watchlistCount, 10)
+  let slides;
+  if (count >= 3) {
+    slides = 3
+  } else if (count === 2) {
+    slides = 2
+  } else {
+    slides = 1
+  }
+
+  $('.followed-watchlists-line-wrapper').slick({
+    infinite: true,
+    slidesToShow: slides,
+    slidesToScroll: slides,
     dots: true
   });
 };
