b9e35d041bd487d9f670992349c8b3c741b0b2e1
Marie Heller <MarieHeller@hotmail.de>
2019-12-10 18:06:07 +0200

    Fixed Top Rated Movies Rating, reviews timing, reviews display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,17 +5,6 @@
 //= require slick.js
 
 
-// if (document.URL.match('watchlists')) {
-// slickFollowed();
-// slick();
-// } else if (document.URL.match('movies')) {
-//   slickMovies();
-// } else if (!document.URL.match(/movies\/\d+/)) {
-//   slick();
-//   slickFriends ();
-//   slickMovies();
-// }
-
 document.addEventListener('DOMContentLoaded', function(){
   if (document.querySelector(".my-watchlists-line-wrapper")) {
     slick();
@@ -25,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function(){
   }
   if (document.querySelector(".followed-watchlists-line-wrapper")) {
     slickFollowed();
-    console.log(8888);
   }
   if (document.querySelector(".movies-line-wrapper")) {
     slickMovies();
